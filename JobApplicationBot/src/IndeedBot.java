de381b13f8fdc4d558f416edd2d56560c865b662
brucetieu <brucetieu5@gmail.com>
2020-09-22 06:41:50 -0600

    added functionality to look for the easily apply and then open those in new tab

--- JobApplicationBot/src/IndeedBot.java
+++ JobApplicationBot/src/IndeedBot.java
@@ -1,11 +1,15 @@
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class IndeedBot {
@@ -13,18 +17,20 @@ public class IndeedBot {
 	WebDriver driver;
 	
     // Member variables
-	String username, password, what, where, url;
+	String url, username, password, what, where, application_type;
 	
 	// No parameter constructor
 	IndeedBot() {}
 	
 	// Constructor with parameters
-	public IndeedBot(String url, String username, String password, String what, String where) {
+	public IndeedBot(String url, String username, String password, String what, String where,
+			String application_type) {
 		this.url = url;
 		this.username = username;
 		this.password = password;
 		this.what = what;
 		this.where = where;
+		this.application_type = application_type;
 	}
 	
 	public void navigateToUrl() {
@@ -59,28 +65,97 @@ public class IndeedBot {
 	
 	public void searchJobs() throws InterruptedException {
 		WebElement findJobsTab = driver.findElement(By.className("gnav-PageLink-text"));
-		WebElement findJobsBtn = driver.findElement(By.className("icl-WhatWhere-buttonWrapper"));
-	
+//		WebElement findJobsTab = driver.findElement(By.xpath("(//a[@data-gnav-action='nav'])[0]"));
+		WebElement findJobsBtn = driver.findElement(By.className("icl-WhatWhere-button"));
+//		WebElement findJobsBtn =  driver.findElement(By.xpath("//div[@class='icl-WhatWhere-buttonWrapper']//button[@type='submit']"));
+
 		// explicit wait -  wait for the "Find Jobs" link to load before clicking it
-		WebDriverWait wait = new WebDriverWait(driver,10);
-		wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("gnav-PageLink-text")));
-
+//		WebDriverWait wait = new WebDriverWait(driver,10);
+//		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("(//span[@class='gnav-PageLink-text'])[0]")));
+//		Thread.sleep(8000);
 		
-        driver.findElement(By.id("text-input-what")).clear();
-        driver.findElement(By.id("text-input-where")).clear();
+		findJobsTab.click();
+		
+		Thread.sleep(8000);
+		
+        WebElement clearWhat = driver.findElement(By.id("text-input-what"));
+        WebElement clearWhere = driver.findElement(By.id("text-input-where"));
+        
+        clearWhat.clear();
+        clearWhat.sendKeys(this.what);
+//        Thread.sleep(3000);
+        clearWhere.clear();
+//        clearWhere.sendKeys(this.where);
+//        Thread.sleep(3000);
+//        driver.findElement(By.xpath("//input[@value='text-input-where']")).clear();
+        clearWhere.submit();
+//        clearWhere.sendKeys(Keys.chord(Keys.CONTROL,"a", Keys.DELETE));
+//        if (clearWhere.getAttribute("value") != "" ) {
+//        	clearWhere.clear();
+//        	
+//        }
+//        clearWhere.sendKeys(this.where);
+        
+//        Thread.sleep(10000);
         
         
-        Thread.sleep(10000);
+//        driver.findElement(By.id("text-input-what")).sendKeys(this.what);
+//        driver.findElement(By.id("text-input-where")).sendKeys(this.where);
+        
+//        Thread.sleep(5000);
+        
+//        findJobsBtn.click();
+//        driver.quit();
+        
 
-        driver.findElement(By.id("text-input-what")).sendKeys(this.what);
-        driver.findElement(By.id("text-input-where")).sendKeys(this.where);
-        
-        Thread.sleep(5000);
-        
-        findJobsBtn.click();
-        
-        
 	}
+	
+	public void JobScrape() throws InterruptedException {
+		
+		// Create a list of JobCards
+		List<WebElement> JobsCard = driver.findElements(By.className("jobsearch-SerpJobCard"));
+		
+		// For each job card
+		for (int i = 0; i < JobsCard.size(); i++) {
+			
+			// Find the links to each job
+			WebElement link = JobsCard.get(i).findElement(By.className("jobtitle"));
+
+			if (this.application_type.toLowerCase() == "easily apply") {
+				// Check if an easily apply exists (has a classname of iaLabel)
+				Boolean isPresent = JobsCard.get(i).findElements(By.className("iaLabel")).size() > 0;
+				
+				// If it does exist, open a new page for each easily apply
+				if (isPresent) {
+					System.out.println("yes");
+					String selectLinkOpeninNewTab = Keys.chord(Keys.COMMAND,Keys.RETURN); 
+					JobsCard.get(i).findElement(By.className("jobtitle")).sendKeys(selectLinkOpeninNewTab);
+				}
+			
+			}
+			
+		}
+
+	
+		
+		
+	}
+	
+	public void quitBrowser() {
+		driver.quit();
+	}
+	
+	public static void main(String[] args) throws InterruptedException {
+		IndeedBot IB = new IndeedBot("https://www.indeed.com/?from=gnav-util-homepage",
+				"email", "password", "Software Developer", "remote", "easily apply");
+		IB.navigateToUrl();
+//		IB.login();
+		IB.searchJobs();
+		IB.JobScrape();
+//		IB.quitBrowser();
+	
+	}
+
 	
 		
 	
