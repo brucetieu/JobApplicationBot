775c232eafff03ffe71d4cd7abe818b14b5eba91
brucetieu <brucetieu5@gmail.com>
2020-09-24 09:54:05 -0600

    Edited BotGUI.java file

--- JobApplicationBot/src/BotGUI.java
+++ JobApplicationBot/src/BotGUI.java
@@ -1,13 +1,30 @@
-
+/**
+ * This is the main class which allows the user to fill in their information before applying. 
+ * TODO: Implement a GUI which allows the user to type in their information.
+ * @author Bruce Tieu
+ */
 public class BotGUI {
-
+	/**
+	 * @param args Not used.
+	 * @throws InterruptedException if the thread executing the method is interrupted, stop the method and return early
+	 */
 	public static void main(String[] args) throws InterruptedException {
-		IndeedBot IB = new IndeedBot("https://www.indeed.com/?from=gnav-util-homepage",
-				"ucdbrucetieu@gmail.com", "password", "Software Developer", "remote");
+		// Create a new object which is a job application data. 
+		JobApplicationData jobAppData = new JobApplicationData();
+		jobAppData.firstname = "John";
+		jobAppData.lastname = "Doe";
+		jobAppData.fullname = "John Doe";
+		jobAppData.email = "ucdbrucetieu@gmail.com";
+		jobAppData.phone = "7202610380";
+		jobAppData.resumePath = "/Users/bruce/Documents/WithObj2_Bruce_Tieu_2020_Resume.pdf";
+		jobAppData.url = "https://www.indeed.com/?from=gnav-util-homepage";
+		jobAppData.password = "password";
+		jobAppData.whatJob = "Software engineer";
+		jobAppData.locationOfJob = "remote";
+		
+		// Create an IndeedBot to apply for jobs.
+		IndeedBot IB = new IndeedBot(jobAppData, JobApplicationData.ApplicationType.EASILY_APPLY);
 		IB.navigateToUrl();
-		IB.login();
 		IB.searchJobs();
-	
+		IB.jobScrape();
 	}
-
-}
