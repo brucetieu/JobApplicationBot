4a896b2a4d19de4f12816fab4ede190e60a4722a
brucetieu <brucetieu5@gmail.com>
2020-09-25 05:02:08 -0600

    Fixed alphabetical imports

--- JobApplicationBot/src/IndeedBot.java
+++ JobApplicationBot/src/IndeedBot.java
@@ -1,17 +1,17 @@
 
 import java.util.ArrayList;
-import java.util.List;
 import java.util.concurrent.TimeUnit;
+import java.util.List;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 /**
  * This class applies to jobs on Indeed.com.
