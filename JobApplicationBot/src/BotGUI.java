3977481ee64e6c13159381eb611eb88d9fcf29e5
brucetieu <brucetieu5@gmail.com>
2020-09-26 10:14:51 -0600

    removed BotGUI.java

--- JobApplicationBot/src/BotGUI.java
+++ JobApplicationBot/src/BotGUI.java
@@ -1,3 +1,6 @@
+import javax.swing.Jframe;
+import javax.swing.jpanel;
+
 /**
  * This is the main class which allows the user to fill in their information before applying. 
  * TODO: Implement a GUI which allows the user to type in their information.
@@ -10,21 +13,28 @@ public class BotGUI {
 	 */
 	public static void main(String[] args) throws InterruptedException {
 		// Create a new object which is a job application data. 
-		JobApplicationData jobAppData = new JobApplicationData();
-		jobAppData.firstname = "John";
-		jobAppData.lastname = "Doe";
-		jobAppData.fullname = "John Doe";
-		jobAppData.email = "ucdbrucetieu@gmail.com";
-		jobAppData.phone = "7202610380";
-		jobAppData.resumePath = "/Users/bruce/Documents/WithObj2_Bruce_Tieu_2020_Resume.pdf";
-		jobAppData.url = "https://www.indeed.com/?from=gnav-util-homepage";
-		jobAppData.password = "password";
-		jobAppData.whatJob = "Software engineer";
-		jobAppData.locationOfJob = "remote";
+//		JobApplicationData jobAppData = new JobApplicationData();
+//		jobAppData.firstname = "John";
+//		jobAppData.lastname = "Doe";
+//		jobAppData.fullname = "John Doe";
+//		jobAppData.email = "ucdbrucetieu@gmail.com";
+//		jobAppData.phone = "7202610380";
+//		jobAppData.resumePath = "/Users/bruce/Documents/WithObj2_Bruce_Tieu_2020_Resume.pdf";
+//		jobAppData.url = "https://www.indeed.com/?from=gnav-util-homepage";
+//		jobAppData.password = "password";
+//		jobAppData.whatJob = "Software engineer";
+//		jobAppData.locationOfJob = "remote";
+//		
+//		// Create an IndeedBot to apply for jobs.
+//		IndeedBot IB = new IndeedBot(jobAppData, JobApplicationData.ApplicationType.EASILY_APPLY);
+//		IB.navigateToUrl();
+//		IB.searchJobs();
+//		IB.jobScrape();
 		
-		// Create an IndeedBot to apply for jobs.
-		IndeedBot IB = new IndeedBot(jobAppData, JobApplicationData.ApplicationType.EASILY_APPLY);
-		IB.navigateToUrl();
-		IB.searchJobs();
-		IB.jobScrape();
+		JFrame panel = new JPanel();
+		JFrame frame = new JFrame();
+		frame.setSize(100,100);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+		frame.add(panel);
 	}
