605ddb74fe520faa59bbf2ce8662871caa76736f
brucetieu <brucetieu5@gmail.com>
2020-10-04 08:24:02 -0600

    added job posting class

--- JobApplicationBot/src/JobApplicationData.java
+++ JobApplicationBot/src/JobApplicationData.java
@@ -1,4 +1,12 @@
-
+/* This class holds all the job application data
+ * 
+ * @author Bruce Tieu
+ */
 public class JobApplicationData {
+    public String firstname, lastname, fullname, email, phone, resumePath, url, password, whatJob, locationOfJob;
+
+    public static enum ApplicationType {
+        EASILY_APPLY, NONE, URGENTLY_HIRING
+    }
 
 }
---
+++ JobApplicationBot/src/JobPostingData.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class JobPostingData {
+
+    public int pageNum;
+    public ArrayList<HashMap<String, String>> jobPosting;
+
+}
