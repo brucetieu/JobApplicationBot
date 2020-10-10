5d5de6ed01b9143dae4c94d6088baeda26910e3f
brucetieu <brucetieu5@gmail.com>
2020-10-10 06:14:55 -0600

    Apply Apache License

--- ParseResume/src/ExtractPDFText.java
+++ ParseResume/src/ExtractPDFText.java
@@ -10,11 +10,23 @@ import org.apache.pdfbox.pdmodel.encryption.AccessPermission;
 import org.apache.pdfbox.text.PDFTextStripper;
 
 /**
+ * Copyright 2020 Bruce Tieu
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License. This is a simple text extraction example to get started.
+ *
  * Adapted from:
  * https://svn.apache.org/viewvc/pdfbox/trunk/examples/src/main/java/org/apache/pdfbox/examples/util/ExtractTextSimple.java?view=markup
  * 
- * This is a simple text extraction example to get started.
- *
  * @author Tilman Hausherr
  */
 public class ExtractPDFText {
