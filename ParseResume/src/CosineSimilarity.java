1f68decfed4360c044b0af1922bed721a996d585
brucetieu <brucetieu5@gmail.com>
2020-10-15 09:27:40 -0600

    Updated CosineSimilarity

--- ParseResume/src/CosineSimilarity.java
+++ ParseResume/src/CosineSimilarity.java
@@ -1,5 +1,5 @@
 
-import java.util.Hashtable;
+import java.io.IOException;
 
 /**
  * Class which computes the cosine similarity of two documents.
@@ -15,16 +15,22 @@ public class CosineSimilarity {
      * @param vectorA The vector of tf-idfs for document 1.
      * @param vectorB The vector of tf-idfs for document 2.
      * @return The cosine similarity of two documents.
+     * @throws IOException Catch any file errors.
      */
-    public double cosineSimilarity(Hashtable<String, Double> vectorA, Hashtable<String, Double> vectorB) {
+    public static double cosineSimilarity(String doc1, String doc2) throws IOException {
 
+        // First get the tf-idf vectors for doc1 and doc2.
+        TFIDFCalc tfidfObj = TFIDFCalc.runTFIDFCalc(doc1, doc2);
+        
         double dotProduct = 0.0;
         double normA = 0.0;
         double normB = 0.0;
-        for (String word : vectorA.keySet()) {
-            dotProduct += vectorA.get(word) * vectorB.get(word);
-            normA += Math.pow(vectorA.get(word), 2);
-            normB += Math.pow(vectorB.get(word), 2);
+        
+        // Then compute the cosine similarity between the documents.
+        for (String word : tfidfObj.vectorA.keySet()) {
+            dotProduct += tfidfObj.vectorA.get(word) * tfidfObj.vectorB.get(word);
+            normA += Math.pow(tfidfObj.vectorA.get(word), 2);
+            normB += Math.pow(tfidfObj.vectorB.get(word), 2);
         }
         return dotProduct / (Math.sqrt(normA) * Math.sqrt(normB));
     }
