d0d077f14adc0122f82e3cfbf5e9b310a3a597e4
brucetieu <brucetieu5@gmail.com>
2020-10-16 10:26:13 -0600

    Updated CosineSimilarity

--- ParseResume/src/CosineSimilarity.java
+++ ParseResume/src/CosineSimilarity.java
@@ -1,5 +1,7 @@
 
 import java.io.IOException;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
 
 /**
  * Class which computes the cosine similarity of two documents.
@@ -12,26 +14,27 @@ public class CosineSimilarity {
     /**
      * This calculates the cosine similarity of two documents.
      * 
-     * @param vectorA The vector of tf-idfs for document 1.
-     * @param vectorB The vector of tf-idfs for document 2.
+     * @param doc1 The raw string contents of document 1.
+     * @param doc2 The raw string contents of document 2.
      * @return The cosine similarity of two documents.
      * @throws IOException Catch any file errors.
      */
-    public static double cosineSimilarity(String doc1, String doc2) throws IOException {
+    public static String cosineSimilarity(String doc1, String doc2) throws IOException {
 
-        // First get the tf-idf vectors for doc1 and doc2.
+        NumberFormat formatter = new DecimalFormat("#0.00");
+        // First, get the tf-idf TextDocuments.
         TFIDFCalc tfidfObj = TFIDFCalc.runTFIDFCalc(doc1, doc2);
-        
+
         double dotProduct = 0.0;
         double normA = 0.0;
         double normB = 0.0;
-        
+
         // Then compute the cosine similarity between the documents.
-        for (String word : tfidfObj.vectorA.keySet()) {
-            dotProduct += tfidfObj.vectorA.get(word) * tfidfObj.vectorB.get(word);
-            normA += Math.pow(tfidfObj.vectorA.get(word), 2);
-            normB += Math.pow(tfidfObj.vectorB.get(word), 2);
+        for (String word : tfidfObj.vectorA.hashTable.keySet()) {
+            dotProduct += tfidfObj.vectorA.hashTable.get(word) * tfidfObj.vectorB.hashTable.get(word);
+            normA += Math.pow(tfidfObj.vectorA.hashTable.get(word), 2);
+            normB += Math.pow(tfidfObj.vectorB.hashTable.get(word), 2);
         }
-        return dotProduct / (Math.sqrt(normA) * Math.sqrt(normB));
+        return formatter.format(dotProduct / (Math.sqrt(normA) * Math.sqrt(normB)));
     }
 }
