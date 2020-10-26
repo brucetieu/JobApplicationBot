a958701b9e319ecd246e33919aef0327c793dfa1
brucetieu <brucetieu5@gmail.com>
2020-10-26 07:02:29 -0600

    Fix file path?

--- JobApplicationBot/src/test/java/com/btieu/JobApplicationBot/TFIDFCalcTest.java
+++ JobApplicationBot/src/test/java/com/btieu/JobApplicationBot/TFIDFCalcTest.java
@@ -28,9 +28,9 @@ public class TFIDFCalcTest {
      * @throws IOException        Catch any file reading errors.
      * @throws URISyntaxException
      */
-    public TFIDFCalcTest() throws IOException {
+    public TFIDFCalcTest() throws IOException, URISyntaxException {
         _wordListTestCases = new TFIDFCalcTestCases();
-        _textDocument1 = new TextDocument(new File(TFIDFCalcTestCases.FILE_PATH));
+        _textDocument1 = new TextDocument(new File("./src/test/resources/com/btieu/JobApplicationBot/TestResume.pdf"));
         _textDocument2 = new TextDocument(TFIDFCalcTestCases.JOB_DESCRIPTION_STRING);
         _tfidfCalc = new TFIDFCalc(_textDocument1, _textDocument2);
         _actualJobDescriptionTF = _tfidfCalc.computeTF(_textDocument1.getWordsFromDocument());
