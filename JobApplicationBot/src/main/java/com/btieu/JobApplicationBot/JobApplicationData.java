ae30e104d3261aadc971fc141ed0b628b9418213
brucetieu <brucetieu5@gmail.com>
2020-11-10 07:37:28 -0700

    Change NONE enum -> NOT_EASY_APPLY enum

--- JobApplicationBot/src/main/java/com/btieu/JobApplicationBot/JobApplicationData.java
+++ JobApplicationBot/src/main/java/com/btieu/JobApplicationBot/JobApplicationData.java
@@ -10,7 +10,7 @@ public class JobApplicationData {
     public static String resumePath;
     
     public static enum ApplicationType {
-        EASILY_APPLY, NONE, URGENTLY_HIRING, ALL
+        EASILY_APPLY, NOT_EASY_APPLY, URGENTLY_HIRING, ALL
     }
 
 }
