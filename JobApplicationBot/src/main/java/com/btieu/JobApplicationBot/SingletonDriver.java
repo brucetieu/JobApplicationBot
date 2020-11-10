b0283c7a563a7fcb5e0bfc672ec5e5c897f91859
brucetieu <brucetieu5@gmail.com>
2020-11-10 07:35:10 -0700

    Add SingleDriver which follows the Singleton pattern

--- JobApplicationBot/src/main/java/com/btieu/JobApplicationBot/SingletonDriver.java
+++ JobApplicationBot/src/main/java/com/btieu/JobApplicationBot/SingletonDriver.java
@@ -1,24 +1,16 @@
 package com.btieu.JobApplicationBot;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.concurrent.TimeUnit;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+
 
 /**
- * This class holds common bot actions like waiting and navigating.
+ * Singleton class - allow only one instance of this class to be created at a time.
  * 
  * @author bruce
  */
@@ -34,7 +26,6 @@ public class SingletonDriver {
     private WebDriverWait _wait;
     private ChromeOptions _chromeOptions;
     private Actions _actions;
-//    private JavascriptExecutor _js;
 
     /**
      * This the default constructor which initializes all the required drivers and
@@ -45,31 +36,25 @@ public class SingletonDriver {
         _chromeOptions = new ChromeOptions();
         _chromeOptions.addArguments("--disable-blink-features");
         _chromeOptions.addArguments("--disable-blink-features=AutomationControlled");
-//        _chromeOptions.addArguments("--headless");
-        // FIXME: Not applicable on every machine.
-//        _chromeOptions.addArguments("--user-data-dir=/Users/bruce/Library/Application Support/Google/Chrome");
+        _chromeOptions.addArguments("--headless"); // Run browser in the background.
         _chromeOptions.addArguments("start-maximized");
         _webDriver = new ChromeDriver(_chromeOptions);
         _webDriver.manage().timeouts().implicitlyWait(_MAX_WAIT_TIME, TimeUnit.SECONDS);
         _wait = new WebDriverWait(_webDriver, _MAX_WAIT_TIME);
         _actions = new Actions(_webDriver);
-//        _js = (JavascriptExecutor) _webDriver;
+
     }
 
+    /**
+     * Ensure only one instance is created.
+     * @return The instance.
+     */
     public static SingletonDriver getInstance() {
         if (singleInstance == null) {
             singleInstance = new SingletonDriver();
         }
         return singleInstance;
     }
-    // Abstract methods
-//    abstract public void navigateToJobPage();
-//    abstract public void login() throws InterruptedException;
-//    abstract public void searchJobs() throws InterruptedException;
-//    abstract public JobPostingData getJobInformation(String jobLink, JobApplicationData.ApplicationType appType,
-//            boolean isApplied) throws IOException;
-//    abstract public void saveJob(String jobLink, JobApplicationData.ApplicationType appType) throws InterruptedException, IOException;
-
     
     /**
      * This is a getter method.
@@ -98,7 +83,4 @@ public class SingletonDriver {
         return _actions;
     }
     
-//    public JavascriptExecutor useJS() {
-//        return _js;
-//    }
 }
