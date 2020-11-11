e42426aa3f62e58dd6b531605699550814021a14
brucetieu <brucetieu5@gmail.com>
2020-11-11 11:41:20 -0700

    Add connect functionality

--- JobApplicationBot/src/main/java/com/btieu/JobApplicationBot/LinkedInBot.java
+++ JobApplicationBot/src/main/java/com/btieu/JobApplicationBot/LinkedInBot.java
@@ -1,17 +1,21 @@
 package com.btieu.JobApplicationBot;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 
 public class LinkedInBot extends Bot {
 
     private JobApplicationData _jobAppData;
+    private List<String> _peopleContainer;
 
     public LinkedInBot(JobApplicationData jobAppData) {
         _jobAppData = jobAppData;
+        _peopleContainer = new ArrayList<String>();
 
     }
 
@@ -42,14 +46,57 @@ public class LinkedInBot extends Bot {
         waitOnElementAndClick(By.className("btn__primary--large"));
     }
 
-    public void search() {
-        WebElement search = tryToFindElement(By.className("search-global-typeahead__input"));
-        search.sendKeys("Software engineer");
-        search.sendKeys(Keys.RETURN);
+    public void search() throws InterruptedException {
+        String peoplePage = "https://www.linkedin.com/search/results/people/?keywords=" + "Software engineer";
+        getDriver().get(peoplePage);
+    }
 
+    public void aggregatePeopleProfiles() throws InterruptedException {
         // Click on "People" button.
-        waitOnElementAndClick(By.className("search-vertical-filter__filter-item"));
-      
+//        waitOnElementAndClick(By.className("search-vertical-filter__filter-item"));
+
+        int i = 1;
+
+        while (i <= JobPostingData.pageNum) {
+
+            i++;
+            System.out.println(i);
+
+            WebElement html = getDriver().findElement(By.tagName("html"));
+            html.sendKeys(Keys.END);
+
+            WebElement ul = tryToFindElement(By.className("search-results__list"));
+            List<WebElement> peopleList = ul.findElements(By.tagName("li"));
+
+            for (WebElement people : peopleList) {
+                String profileID = people.findElement(By.tagName("a")).getAttribute("href");
+                _peopleContainer.add(profileID);
+                System.out.println(profileID);
+            }
+
+            String peoplePage = "https://www.linkedin.com/search/results/people/?keywords=" + "computer science student"
+                    + "&page=" + Integer.toString(i);
+            getDriver().get(peoplePage);
+        }
+
+    }
+
+    public void connect() {
+        for (String person : _peopleContainer) {
+            try {
+                getDriver().get(person);
+                waitOnElementAndClick(By.className("pv-s-profile-actions--connect")); // click on Connect
+                waitOnElementAndClick(By.className("artdeco-button--secondary")); // Click on "Add a note"
+                WebElement textarea = tryToFindElement(By.id("custom-message"));
+                textarea.sendKeys(
+                        "Hello, I came across your profile and wanted to connect with people who I aspire to be and share common career interests with - a Software Engineer! Kindly, accept my invitation. Thanks!");
+                waitOnElementAndClick(By.className("ml1")); // Click on Send"
+                System.out.println("Sent invitation!");
+            } catch (Exception e) {
+                continue;
+            }
+
+        }
     }
 
 }
