7c4a41e860176bab9f9d0f4716cae05eac0f0735
brucetieu <brucetieu5@gmail.com>
2020-11-11 05:47:15 -0700

    Create Pagination

---
+++ JobApplicationBot/src/main/java/com/btieu/JobApplicationBot/Pagination.java
@@ -0,0 +1,49 @@
+package com.btieu.JobApplicationBot;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+/**
+ * Define methods to click on next pages and implement this interface with a
+ * lambda expression.
+ * 
+ * @author Bruce Tieu
+ *
+ */
+class Pagination {
+    private JobApplicationData _jobAppData;
+    private Bot _bot;
+
+    /**
+     * Parameterized constructor to initialize JobApplicationData and Bot object.
+     * 
+     * @param jobAppData The JobApplicationData.
+     */
+    public Pagination(JobApplicationData jobAppData) {
+        _jobAppData = jobAppData;
+        _bot = new Bot();
+    }
+
+    /**
+     * Go to next page listing in Indeed.
+     * 
+     * @param pageNum The desired number of page numbers to search.
+     * @return An updated list of jobs to be iterated over again.
+     */
+    public List<WebElement> goToNextIndeedPage(int pageNum) {
+
+        String nextPageUrl = "https://www.indeed.com/jobs?q=" + _jobAppData.whatJob + "&l=" + _jobAppData.locationOfJob
+                + "&start=" + pageNum * 10;
+        System.out.println("Continuing search on next page...");
+        _bot.getWebDriver().get(nextPageUrl);
+
+        // Click out of potential pop ups.
+        _bot.getActions().moveByOffset(0, 0).click().build().perform();
+        _bot.getWebDriver().switchTo().defaultContent();
+
+        return _bot.tryToFindElements(By.className("jobsearch-SerpJobCard"));
+    }
+}
+
