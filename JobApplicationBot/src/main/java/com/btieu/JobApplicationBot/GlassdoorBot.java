8ef0bf429cc291c131ad543fe50fef0f59c15813
brucetieu <brucetieu5@gmail.com>
2020-11-12 05:08:41 -0700

    Add saveJob() and getJobInformation() methods

--- JobApplicationBot/src/main/java/com/btieu/JobApplicationBot/GlassdoorBot.java
+++ JobApplicationBot/src/main/java/com/btieu/JobApplicationBot/GlassdoorBot.java
@@ -1,6 +1,8 @@
 package com.btieu.JobApplicationBot;
 
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 import org.openqa.selenium.By;
@@ -93,6 +95,65 @@ public class GlassdoorBot extends Bot {
         navigateToLinkInNewTab(href); // Open that job in a new tab.
         return href;
     }
+    
+    /**
+     * Save each job to a container.
+     * 
+     * @param jobLink The application page.
+     * @param appType The type of application it was.
+     * @throws InterruptedException Catch any elements that are not found.
+     * @throws IOException          Catch and file writing errors.
+     */
+    public void saveJob(String jobLink, JobApplicationData.ApplicationType appType)
+            throws InterruptedException, IOException {
+        
+        boolean hasJobInContainer = JobPostingData.jobPostingContainer.contains(getJobInformation(jobLink, appType, false));
+
+        if (!hasJobInContainer) {
+            JobPostingData.jobPostingContainer.add(getJobInformation(jobLink, appType, false)); // Save job.
+            getWebDriver().close(); // Close that new window (the job that was opened).
+            getWebDriver().switchTo().window(_parentWindow); // Switch back to the parent window (job listing window).
+        }
+    }
+
+    /**
+     * Scrape the job view page for the company name, job title, location, if it was
+     * applied to, and the job status.
+     */
+    public JobPostingData getJobInformation(String jobLink, JobApplicationData.ApplicationType appType, boolean applied)
+            throws IOException {
+
+        String remote, submitted;
+
+        Date date = new Date();
+        SimpleDateFormat formatter = new SimpleDateFormat("MM-dd-yyyy HH:mm");
+
+        WebElement jobContainer = tryToFindElement(By.id("JobView"));
+        WebElement empInfo = jobContainer.findElement(By.className("smarterBannerEmpInfo"));
+        WebElement flexColumn = empInfo.findElement(By.className("flex-column"));
+        WebElement div = flexColumn.findElement(By.tagName("div"));
+        List<WebElement> divs = div.findElements(By.tagName("div"));
+        WebElement companyName = divs.get(0);
+
+        String companyNameString = getTextExcludingChildren(companyName);
+        String jobTitleString = divs.get(1).getText();
+        String jobLocationString = divs.get(2).getText();
+
+        if (jobLocationString.toLowerCase().contains("remote"))
+            remote = "yes";
+        else
+            remote = "no";
+
+        if (applied)
+            submitted = "yes";
+        else
+            submitted = "no";
+
+        // Return a new JobPostingData object.
+        return new JobPostingData(jobMatchScore(By.id("JobDescriptionContainer")), jobTitleString, companyNameString,
+                jobLocationString, remote, formatter.format(date), appType.name(), jobLink, submitted, "");
+    }
+
 
 
     
