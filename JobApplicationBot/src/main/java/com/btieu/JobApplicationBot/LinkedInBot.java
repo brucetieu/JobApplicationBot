5bae6121878f9cc236c7ca1e9e9381ea868c82a8
brucetieu <brucetieu5@gmail.com>
2020-11-15 08:52:35 -0700

    Add _splitFullName(), _assembleMessage(), _getPeopleViewed()

--- JobApplicationBot/src/main/java/com/btieu/JobApplicationBot/LinkedInBot.java
+++ JobApplicationBot/src/main/java/com/btieu/JobApplicationBot/LinkedInBot.java
@@ -56,6 +56,41 @@ public class LinkedInBot extends Bot {
     public void goToProfile() {
         getWebDriver().get(_jobAppData.linkedin);
     }
+    
+    private void _getPeopleViewed() {
+        WebElement pvContainer = tryToFindElement(By.className("pv-browsemap-section"));
+        List<WebElement> pvList = pvContainer.findElements(By.className("pv-browsemap-section__member-container"));
+
+        for (WebElement people : pvList) {
+            String profileLink = people.findElement(By.tagName("a")).getAttribute("href");
+            String name = people.findElement(By.className("name")).getText();
+            String occupation = people.findElement(By.className("pv-browsemap-section__member-headline")).getText();
+            if (!_profilesToBeVisited.stream().anyMatch(l -> l.profileLink.equals(profileLink))
+                    && !_visitedProfiles.contains(profileLink)) {
+                _profilesToBeVisited.add(new LinkedInPerson(_splitFullname(name), profileLink, occupation,
+                        _assembleMessage(_splitFullname(name))));
+            }
+        }
+    }
+
+
+    private String _splitFullname(String name) {
+        String[] splitted = name.toLowerCase().split("\\s+");
+        String firstName = Character.toUpperCase(splitted[0].charAt(0)) + splitted[0].substring(1);
+        return firstName;
+        
+    }
+    
+    private String _assembleMessage(String name) {
+        String message = "Hi " + name + ", your profile appeared in my search of software engineers. "
+                + "I am currently pursuing a career in software engineering and "
+                + "it would be great to hear about your journey and experience in the field. "
+                + "Kindly, accept my invitation. You would be a big help! Sincerely, " + _jobAppData.fullname + ".";
+
+        return message;
+    }
+
+
 
   
 
