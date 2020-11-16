951c1fe37189894cb03487a7f8d564a960ce9f40
brucetieu <brucetieu5@gmail.com>
2020-11-16 08:00:28 -0700

    Add comments

--- JobApplicationBot/src/main/java/com/btieu/JobApplicationBot/LinkedInPerson.java
+++ JobApplicationBot/src/main/java/com/btieu/JobApplicationBot/LinkedInPerson.java
@@ -1,18 +1,33 @@
 package com.btieu.JobApplicationBot;
 
+/**
+ * Assemble a Linkedin Profile / person.
+ * @author Bruce Tieu
+ *
+ */
 public class LinkedInPerson {
     public String firstname, profileLink, occupation, message, keywords;
     public static int MAX_CONNECTIONS;
     
+    /**
+     * Initialize name, profile link, occupation, and the message.
+     * @param firstname The person's first name.
+     * @param profileLink Their profile link.
+     * @param occupation Their occupation.
+     * @param message The message to be sent.
+     */
     public LinkedInPerson(String firstname, String profileLink, String occupation, String message) {
         this.firstname = firstname;
         this.profileLink = profileLink;
         this.occupation = occupation;
         this.message = message;
     }
-    
-    public LinkedInPerson() {}
-    
-  
+
+    /**
+     * Default constructor.
+     */
+    public LinkedInPerson() {
+
+    }
 }
 
