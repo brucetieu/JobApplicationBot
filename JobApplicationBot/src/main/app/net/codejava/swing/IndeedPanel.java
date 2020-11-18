f52adabb3e1e0cc36dea75c1f90ec8f6da9b2211
brucetieu <brucetieu5@gmail.com>
2020-11-18 07:11:06 -0700

    Refactored createIndeedPanel()

--- JobApplicationBot/src/main/app/net/codejava/swing/IndeedPanel.java
+++ JobApplicationBot/src/main/app/net/codejava/swing/IndeedPanel.java
@@ -50,37 +50,10 @@ public class IndeedPanel extends CreateGUIComponents {
      */
     public void createIndeedPanel(JPanel _contentPane) {
 
-        createTab("Indeed", _contentPane, _tabbedPane, 0, 0, 650, 650);
-        createGoodiesTitle("Indeed Login Info", 20, 32, 231, 16);
-
-        // Applicant information fields.
-        addLabels("First name", 20, 65, 100, 16);
-        addLabels("Last name", 20, 97, 100, 16);
-        addLabels("Full name", 20, 128, 100, 16);
-        addLabels("Email", 20, 166, 61, 16);
-        addLabels("Password", 20, 206, 61, 16);
-        addLabels("Phone number", 20, 244, 91, 16);
-
-        _firstName = addTextField(125, 60, 130, 26, 10);
-        _lastName = addTextField(125, 92, 130, 26, 10);
-        _fullName = addTextField(125, 123, 130, 26, 10);
-        _email = addTextField(125, 161, 130, 26, 10);
-
-        _phoneNumber = addTextField(125, 239, 130, 26, 10);
-        _password = addPasswordField(125, 201, 130, 26, 10);
-
-        // Job preferences column.
-        createGoodiesTitle("Job Preferences", 391, 32, 122, 16);
-        addLabels("What job", 285, 65, 61, 16);
-        addLabels("Location of job", 285, 97, 100, 16);
-        addLabels("Application type", 285, 128, 150, 16);
-
-        addDropDown(_options, 401, 124, 150, 27);
-
-        _whatJob = addTextField(401, 60, 130, 26, 10);
-        _jobLoc = addTextField(401, 92, 130, 26, 10);
-
-        addUploadResume(191, 295, 300, 16);
+        createTab("Indeed", _contentPane, _tabbedPane, 0, 0, 0, 0);
+        _addApplicantFields(); // Applicant info fields.
+        _addJobPreferenceFields(); // Job preferences fields.
+        addUploadResume(285, 275, 200, 29);
     }
 
     /**
