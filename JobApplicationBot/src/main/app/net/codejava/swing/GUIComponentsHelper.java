48aa3bbdeb56c9d3b05b147c1dd22ac59404692e
brucetieu <brucetieu5@gmail.com>
2020-11-19 09:13:38 -0700

    Pass in a starting page num instead of hard coding it

--- JobApplicationBot/src/main/app/net/codejava/swing/GUIComponentsHelper.java
+++ JobApplicationBot/src/main/app/net/codejava/swing/GUIComponentsHelper.java
@@ -19,13 +19,14 @@ public class GUIComponentsHelper {
      * Generate page numbers to add as a dropdown.
      * @return An Integer array page numbers.
      */
-    public static Integer[] generatePageNumbers() {
+    public static Integer[] generatePageNumbers(int start) {
         Integer pageNumContainer[] = new Integer[_MAX_PAGE_NUM];
-        for (int i = 0; i < _MAX_PAGE_NUM; i++) {
+        for (int i = start; i < _MAX_PAGE_NUM; i++) {
             pageNumContainer[i] = i;
         }
         return pageNumContainer;
     }
+    
     
     /**
      * Format phone number to be #########.
