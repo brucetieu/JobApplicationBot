cab1c5bd77f2f7c02f52219955bbed6674611a2f
brucetieu <brucetieu5@gmail.com>
2020-11-20 11:12:58 -0700

    Add license

--- JobApplicationBot/src/main/app/net/codejava/swing/CreateGUIComponents.java
+++ JobApplicationBot/src/main/app/net/codejava/swing/CreateGUIComponents.java
@@ -1,3 +1,35 @@
+/*
+ * Copyright (c) 1995, 2008, Oracle and/or its affiliates. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *   - Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *
+ *   - Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *
+ *   - Neither the name of Oracle or the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+
 package net.codejava.swing;
 
 import com.btieu.JobApplicationBot.JobApplicationData;
@@ -15,10 +47,12 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JPasswordField;
+import javax.swing.JScrollPane;
 import javax.swing.JTabbedPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
+import javax.swing.text.AbstractDocument;
 
 /**
  * This class contains the methods used to create swing panels, labels, buttons,
@@ -43,7 +77,6 @@ public class CreateGUIComponents extends JFrame {
     private SingletonTab _singletonTab;
     private JTextArea _textArea;
     private JTextArea _changeLog;
-    private String newline = "\n";
 
 
     /**
@@ -124,9 +157,9 @@ public class CreateGUIComponents extends JFrame {
         _changeLog = new JTextArea(5, 30);
         _changeLog.setEditable(false);
         JScrollPane scrollPaneForLog = new JScrollPane(_changeLog);
-        scrollPaneForLog.setBounds(20, 450, 400, 100);
+        scrollPaneForLog.setBounds(20, 450, 300, 50);
 
-        pDoc.addDocumentListener(new MyDocumentListener());
+        pDoc.addDocumentListener(new ADocumentListener(_changeLog));
 
         _panel.add(scrollPaneForLog);
         _panel.add(_textArea);
