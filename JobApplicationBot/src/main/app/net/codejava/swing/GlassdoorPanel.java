91051a779bc94362432f2d367d7271009da4291c
brucetieu <brucetieu5@gmail.com>
2020-11-24 07:33:16 -0700

    require all fields to be completed

--- JobApplicationBot/src/main/app/net/codejava/swing/GlassdoorPanel.java
+++ JobApplicationBot/src/main/app/net/codejava/swing/GlassdoorPanel.java
@@ -36,28 +36,15 @@ public class GlassdoorPanel extends CreateGUIComponents {
     private static final String _GLASSDOOR_URL = "https://www.glassdoor.com/index.htm";
     private static final int _STARTING_PAGE = 0;
 
-    private JTextField _firstName;
-    private JTextField _lastName;
-    private JTextField _fullName;
     private JTextField _email;
-    private JTextField _phoneNumber;
     private JPasswordField _password;
-//    private JTextField _school;
-    private JTextField _location;
-//    private JTextField _company;
-//    private JTextField _linkedIn;
-//    private JTextField _github;
-//    private JTextField _portfolio;
     private JTextField _whatJob;
     private JTextField _jobLoc;
     private JTextField _csvOutputPath;
     private JComboBox<ApplicationType> _appBox;
     private JComboBox<Integer> _pageNumBox;
     private JTabbedPane _tabbedPane;
-    private List<JTextField> _listOfLGFields = new ArrayList<>();
-    private List<JTextField> _listOfNonLGFields = new ArrayList<>();
-    
-    private static final JPanel cards = new JPanel(new CardLayout());
+    private List<JTextField> _listOfTextFields = new ArrayList<>();
 
     /**
      * Create the Glassdoor panel.
@@ -81,43 +68,28 @@ public class GlassdoorPanel extends CreateGUIComponents {
         JButton launchButton = addButton("Launch", 245, 525, 117, 29);
 
         launchButton.setEnabled(false);
-        
        
-        _validateTextFields(launchButton, _appBox);
+        _validateTextFields(launchButton);
 
         launchButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 JobApplicationData jobAppData = new JobApplicationData();
                 WriteFiles writeFiles = null;
+                
                 try {
                     writeFiles = new WriteFiles(_csvOutputPath.getText());
                 } catch (IOException e2) {
                     e2.printStackTrace();
                 }
-//                jobAppData.firstname = _firstName.getText();
-//                jobAppData.lastname = _lastName.getText();
-//                jobAppData.fullname = _fullName.getText();
+
+                jobAppData.platformUrl = _GLASSDOOR_URL;
                 jobAppData.email = _email.getText();
                 jobAppData.password = String.valueOf(_password.getPassword());
-//                jobAppData.school = _school.getText();
-//                jobAppData.location = _location.getText();
-//                jobAppData.currentCompany = _company.getText();
-//                jobAppData.linkedin = _linkedIn.getText();
-//                jobAppData.github = _github.getText();
-//                jobAppData.portfolio = _portfolio.getText();
-
-//                jobAppData.phone = null;
-//                try {
-//                    jobAppData.phone = GUIComponentsHelper.phoneNumFormatter(_phoneNumber.getText());
-//                } catch (ParseException e1) {
-//                    e1.printStackTrace();
-//                }
-                
-                JobApplicationData.resumePath = getResumeFile().toString();
-                jobAppData.platformUrl = _GLASSDOOR_URL;
-
                 jobAppData.whatJob = _whatJob.getText();
                 jobAppData.locationOfJob = _jobLoc.getText();
+                
+                JobApplicationData.resumePath = getResumeFile().toString();
+               
                 JobPostingData.pagesToScrape = Integer.parseInt(_pageNumBox.getSelectedItem().toString());
                 ApplicationType appType = (ApplicationType) _appBox.getSelectedItem();
                 JobIterator jobIterator = new JobIterator(writeFiles, appType);
@@ -136,32 +108,9 @@ public class GlassdoorPanel extends CreateGUIComponents {
      */
     private void _addApplicantFields() {
 
-//        createGoodiesTitle("Fill below for LEVER_GREENHOUSE", 10, 33, 250, 16);
-//        addLabels("First name", 20, 65, 100, 16);
-//        addLabels("Last name", 20, 97, 100, 16);
-//        addLabels("Full name", 20, 128, 100, 16);
-//        addLabels("Phone number", 20, 161, 100, 16);
-//        addLabels("School", 20, 193, 100, 16);
-//        addLabels("Location", 20, 225, 91, 16);
-//        addLabels("Company", 20, 257, 91, 16);
-//        addLabels("LinkedIn", 20, 289, 91, 16);
-//        addLabels("GitHub", 20, 321, 91, 16);
-//        addLabels("Portfolio", 20, 353, 91, 16);
-
         createGoodiesTitle("Glassdoor Login Info", 230, 232, 175, 16);
         addLabels("Email", 150, 270, 61, 16);
         addLabels("Password", 150, 308, 61, 16);
-
-//        _firstName = addTextField(125, 60, 130, 26, 10);
-//        _lastName = addTextField(125, 92, 130, 26, 10);
-//        _fullName = addTextField(125, 123, 130, 26, 10);
-//        _phoneNumber = addTextField(125, 156, 130, 26, 10);
-//        _school = addTextField(125, 188, 130, 26, 10);
-//        _location = addTextField(125, 220, 130, 26, 10);
-//        _company = addTextField(125, 252, 130, 26, 10);
-//        _linkedIn = addTextField(125, 284, 130, 26, 10);
-//        _github = addTextField(125, 316, 130, 26, 10);
-//        _portfolio = addTextField(125, 348, 130, 26, 10);
 
         _email = addTextField(280, 265, 130, 26, 10);
         _password = addPasswordField(280, 303, 130, 26, 10);
@@ -185,98 +134,23 @@ public class GlassdoorPanel extends CreateGUIComponents {
         _pageNumBox = addDropdown(GUIComponentsHelper.generatePageNumbers(_STARTING_PAGE), 280, 156, 150, 27);
         _csvOutputPath = addTextField(280, 192, 180, 26, 10);
     }
-
-    private void _validateTextFields(JButton launchButton, JComboBox<ApplicationType> comboBox) {
-        comboBox.addActionListener(new ActionListener() {
-
-            @Override
-            public void actionPerformed(ActionEvent e) {
-//                JComboBox<ApplicationType> comboBox = (JComboBox<ApplicationType>) e.getSource();
-                Object selected = comboBox.getSelectedItem();
-                launchButton.setEnabled(false);
-                if (selected == ApplicationType.LEVER_GREENHOUSE) {
-                    _addApplicantFields();
-                
-                    _listOfLGFields.add(_firstName);
-                    _listOfLGFields.add(_lastName);
-                    _listOfLGFields.add(_fullName);
-                    _listOfLGFields.add(_phoneNumber);
-                    _listOfLGFields.add(_location);
-                    
-                    // Disable launch button if any text fields are blank.
-                    for (JTextField tf : _listOfLGFields) {
-                        tf.getDocument().addDocumentListener(new TextfieldListener(_listOfLGFields, launchButton));
-                    }
-                }
-                else {
-                   
-                  
-                    _listOfNonLGFields.add(_whatJob);
-                    _listOfNonLGFields.add(_jobLoc);
-                    _listOfNonLGFields.add(_csvOutputPath);
-                    _listOfNonLGFields.add(_email);
-                    _listOfNonLGFields.add(_password);
-                    
-                    // Disable launch button if any text fields are blank.
-                    for (JTextField tf : _listOfNonLGFields) {
-                        tf.getDocument().addDocumentListener(new TextfieldListener(_listOfNonLGFields, launchButton));
-                    }
-                }
-            }
-            
-        });
+    
+    /**
+     * Check if the text fields are completed by listening to each one.
+     */
+    private void _validateTextFields(JButton launchButton) {
+        
+        // Add text field to the list of text fields.
+        _listOfTextFields.add(_email);
+        _listOfTextFields.add(_password);
+        _listOfTextFields.add(_whatJob);
+        _listOfTextFields.add(_jobLoc);
+        _listOfTextFields.add(_csvOutputPath);
+        
+        // Disable launch button if any text fields are blank.
+        for (JTextField tf : _listOfTextFields) {
+            tf.getDocument().addDocumentListener(new TextfieldListener(_listOfTextFields, launchButton));
+        }
     }
-        
-//        private void create() {
-//            JFrame f = new JFrame();
-//            f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-////            for (int i = 1; i < 9; i++) {
-////                CardPanel p = new CardPanel("Panel " + String.valueOf(i));
-////                combo.addItem(p);
-////                cards.add(p, p.toString());
-////            }
-//            JPanel control = new JPanel();
-//            _appBox.addActionListener(new ActionListener() {
-//
-//                @Override
-//                public void actionPerformed(ActionEvent e) {
-//                    JComboBox<ApplicationType> jcb = (JComboBox<ApplicationType>) e.getSource();
-//                    CardLayout cl = (CardLayout) cards.getLayout();
-//                    cl.show(cards, jcb.getSelectedItem().toString());
-//                }
-//            });
-//            control.add(_appBox);
-//            f.add(cards, BorderLayout.CENTER);
-//            f.add(control, BorderLayout.SOUTH);
-//            f.pack();
-//            f.setLocationRelativeTo(null);
-//            f.setVisible(true);
-//        }
-        
-//        if (appType == ApplicationType.LEVER_GREENHOUSE) {
-//            _listOfLGFields.add(_firstName);
-//            _listOfLGFields.add(_lastName);
-//            _listOfLGFields.add(_fullName);
-//            _listOfLGFields.add(_phoneNumber);
-//            _listOfLGFields.add(_location);
-//            
-//            // Disable launch button if any text fields are blank.
-//            for (JTextField tf : _listOfLGFields) {
-//                tf.getDocument().addDocumentListener(new TextfieldListener(_listOfLGFields, launchButton));
-//            }
-//        }
-//        else {
-//            _listOfNonLGFields.add(_whatJob);
-//            _listOfNonLGFields.add(_jobLoc);
-//            _listOfNonLGFields.add(_csvOutputPath);
-//            _listOfNonLGFields.add(_email);
-//            _listOfNonLGFields.add(_password);
-//            
-//            // Disable launch button if any text fields are blank.
-//            for (JTextField tf : _listOfNonLGFields) {
-//                tf.getDocument().addDocumentListener(new TextfieldListener(_listOfNonLGFields, launchButton));
-//            }
-//        }
-//    }
 
 }
