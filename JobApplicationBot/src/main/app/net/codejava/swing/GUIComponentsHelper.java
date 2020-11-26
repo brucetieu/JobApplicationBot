f2136a67c1cc2ed39d1f1ebab094ab6bdb24dbe4
brucetieu <brucetieu5@gmail.com>
2020-11-26 12:32:32 -0700

    Add just connection container so that 1 shows first

--- JobApplicationBot/src/main/app/net/codejava/swing/GUIComponentsHelper.java
+++ JobApplicationBot/src/main/app/net/codejava/swing/GUIComponentsHelper.java
@@ -27,8 +27,8 @@ public class GUIComponentsHelper {
      */
     public static Integer[] generateMaxConnectRequests() {
         Integer maxConnectContainer[] = new Integer[_MAX_CONNECTION_REQUESTS + 1];
-        for (int i = 1; i <= _MAX_CONNECTION_REQUESTS; i++) {
-            maxConnectContainer[i] = i;
+        for (int i = 0; i <= _MAX_CONNECTION_REQUESTS; i++) {
+            maxConnectContainer[i] = i + 1;
         }
         return maxConnectContainer;
     }
