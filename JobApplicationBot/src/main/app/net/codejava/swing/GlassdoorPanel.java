d43e0f5650025e8c1bfd2590c498e468534a4b73
brucetieu <brucetieu5@gmail.com>
2020-11-26 09:19:53 -0700

    Remove commented out logic in launchApp()

--- JobApplicationBot/src/main/app/net/codejava/swing/GlassdoorPanel.java
+++ JobApplicationBot/src/main/app/net/codejava/swing/GlassdoorPanel.java
@@ -48,6 +48,9 @@ public class GlassdoorPanel extends CreateGUIComponents {
     private Pagination _page;
     private ApplicationType _appType;
 
+    /**
+     * Initialize a JobApplicationData.
+     */
     public GlassdoorPanel() {
         _jobAppData = new JobApplicationData();
     }
@@ -79,41 +82,9 @@ public class GlassdoorPanel extends CreateGUIComponents {
 
         launchButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-//                JobApplicationData jobAppData = new JobApplicationData();
-//                WriteFiles writeFiles = null;
-//
-//                try {
-//                    writeFiles = new WriteFiles(_csvOutputPath.getText());
-//                } catch (IOException e2) {
-//                    e2.printStackTrace();
-//                }
-//
-//                jobAppData.platformUrl = _GLASSDOOR_URL;
-//                jobAppData.email = _email.getText();
-//                jobAppData.password = String.valueOf(_password.getPassword());
-//                jobAppData.whatJob = _whatJob.getText();
-//                jobAppData.locationOfJob = _jobLoc.getText();
-//
-//                // Validate the email.
-//                if (!_emailValidator.validate(jobAppData.email.trim())) {
-//                    MessageDialog.infoBox("Invalid email format! Try again.", "Invalid email");
-//                    return;
-//                }
-//
-//                // Verify a resume has been uploaded.
-//                try {
-//                    JobApplicationData.resumePath = getResumeFile().toString();
-//                } catch (Exception e1) {
-//                    MessageDialog.infoBox("Please upload a resume! Try again.", "No resume supplied");
-//                    return;
-//                }
-//
-//                JobPostingData.pagesToScrape = Integer.parseInt(_pageNumBox.getSelectedItem().toString());
-//                ApplicationType appType = (ApplicationType) _appBox.getSelectedItem();
-//                JobIterator jobIterator = new JobIterator(writeFiles, appType);
-//                Pagination page = new Pagination(jobAppData);
-
+                           
                 _getCompleteFields();
+                
                 // Run the GlassdoorBot
                 new RunGlassdoorBot(_appType, _jobAppData, _jobIterator, _page, _writeFiles);
 
@@ -175,10 +146,18 @@ public class GlassdoorPanel extends CreateGUIComponents {
     private void _getCompleteFields() {
         _writeFiles = null;
         
+        // Validate the csv output is actually a csv.
         try {
-            _writeFiles = new WriteFiles(_csvOutputPath.getText());
+            if (_csvOutputPath.getText().endsWith(".csv") && _csvOutputPath.getText().length() > 4) {
+                _writeFiles = new WriteFiles(_csvOutputPath.getText());
+            }
+            else {
+                MessageDialog.infoBox(MessageDialog.INVALID_CSV_MSG, MessageDialog.INVALID_CSV_TITLE);
+                return;
+            }
+           
         } catch (IOException e2) {
-            e2.printStackTrace();
+            System.out.println(e2.toString());
         }
         
         _jobAppData.platformUrl = _GLASSDOOR_URL;
@@ -189,7 +168,7 @@ public class GlassdoorPanel extends CreateGUIComponents {
 
         // Validate the email.
         if (!_emailValidator.validate(_jobAppData.email.trim())) {
-            MessageDialog.infoBox(MessageDialog.VALIDATE_EMAIL_MSG, MessageDialog.INVALID_EMAIL_TITLE);
+            MessageDialog.infoBox(MessageDialog.INVALID_EMAIL_MSG, MessageDialog.INVALID_EMAIL_TITLE);
             return;
         }
 
@@ -197,7 +176,7 @@ public class GlassdoorPanel extends CreateGUIComponents {
         try {
             JobApplicationData.resumePath = getResumeFile().toString();
         } catch (Exception e1) {
-            MessageDialog.infoBox(MessageDialog.VALIDATE_RESUME_MSG, MessageDialog.NO_RESUME_TITLE);
+            MessageDialog.infoBox(MessageDialog.NO_RESUME_MSG, MessageDialog.NO_RESUME_TITLE);
             return;
         }
 
