769f90dc320f4b85be77f136af97ceb095809ff1
brucetieu <brucetieu5@gmail.com>
2020-11-26 11:37:42 -0700

    Removed phone masker which wasn't working

--- JobApplicationBot/src/main/app/net/codejava/swing/GUIComponentsHelper.java
+++ JobApplicationBot/src/main/app/net/codejava/swing/GUIComponentsHelper.java
@@ -12,9 +12,6 @@ public class GUIComponentsHelper {
     
     private static final int _MAX_PAGE_NUM = 50;
     private static final int _MAX_CONNECTION_REQUESTS = 500;
-    private static MaskFormatter _maskFormatter;
-    
-    public static final String PHONE_MASK = "##########";
     
     /**
      * Generate page numbers to add as a dropdown.
@@ -40,17 +37,5 @@ public class GUIComponentsHelper {
         return maxConnectContainer;
     }
     
-    /**
-     * Format phone number to be #########.
-     * @param phoneNumber The phone number in a String format.
-     * @return The formatted phone number.
-     * @throws ParseException Catch any parsing errors. 
-     */
-    public static String phoneNumFormatter(String phoneNumber) throws ParseException {
-        _maskFormatter = new MaskFormatter(PHONE_MASK);
-        _maskFormatter.setValueContainsLiteralCharacters(false);
-        return _maskFormatter.valueToString(phoneNumber);
-        
-    }
     
 }
