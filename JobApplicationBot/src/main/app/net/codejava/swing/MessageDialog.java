a75cfbecf20fa72e474b1e11abf2959861a53190
brucetieu <brucetieu5@gmail.com>
2020-11-26 12:34:09 -0700

    Add additional message dialogs for linkedin, levergreenhouse

--- JobApplicationBot/src/main/app/net/codejava/swing/MessageDialog.java
+++ JobApplicationBot/src/main/app/net/codejava/swing/MessageDialog.java
@@ -2,6 +2,8 @@ package net.codejava.swing;
 
 import javax.swing.JOptionPane;
 
+import com.btieu.JobApplicationBot.LinkedInPerson;
+
 /**
  * Generate a message dialog as the user interacts with the GUI.
  * @author bruce
@@ -17,9 +19,15 @@ public class MessageDialog {
     public static final String GLASSDOOR_NOT_EASY_APPLY_MSG = "Saving Glassdoor Not Easily Apply jobs... Click Ok to continue.";
     public static final String GLASSDOOR_ALL_MSG = "Saving all Glasdoor jobs... Click Ok to continue.";
     
+    public static final String LEVER_GREENHOUSE_MSG = "Saving and Applying to Lever / Greenhouse jobs... Click Ok to continue.";
+    
+    public static final String LINKEDIN_MSG = "Sending connection requests... Click Ok to continue.";
+    public static final String LINKEDIN_NO_RESULTS_MSG = "No people to connect with those keywords. Try again!";
+   
     public static final String SUCCESS_JOB_SAVE_MSG = "Job successfully saved. You may search again with a different config or close the app.";
     public static final String SUCCESS_TITLE = "Success";
     public static final String SUCCESSFUL_LAUNCH_TITLE = "Successful launch";
+    public static final String SUCCESSFUL_CONNECT_REQUEST = "Successfully sent " + LinkedInPerson.MAX_CONNECTIONS + " connect requests.";
     
     public static final String INVALID_EMAIL_MSG = "Invalid email format! Try again.";
     public static final String INVALID_EMAIL_TITLE = "Invalid email";
@@ -33,6 +41,8 @@ public class MessageDialog {
     public static final String INVALID_PHONE_MSG = "Incorrect phone number format! Try again.";
     public static final String INVALID_PHONE_TITLE = "Invalid phone number format";
     
+    public static final String INVALID_KEYWORD_TITLE = "No keyword matches found.";
+    
 
     /**
      * Generate messages for the message dialog box.
@@ -42,4 +52,5 @@ public class MessageDialog {
     public static void infoBox(String infoMessage, String titleBar) {
         JOptionPane.showMessageDialog(null, infoMessage, "InfoBox: " + titleBar, JOptionPane.INFORMATION_MESSAGE);
     }
+    
 }
