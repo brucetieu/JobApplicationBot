a01068c3500e1c0f11b6092f6c242f3cbdd3fc80
brucetieu <brucetieu5@gmail.com>
2020-11-27 10:36:28 -0700

    Add error message dialogs

--- JobApplicationBot/src/main/app/net/codejava/swing/MessageDialog.java
+++ JobApplicationBot/src/main/app/net/codejava/swing/MessageDialog.java
@@ -43,6 +43,9 @@ public class MessageDialog {
     
     public static final String INVALID_KEYWORD_TITLE = "No keyword matches found.";
     
+    public static final String ERROR_RUNNING_BOT_MSG = "Error running the bot.";
+    public static final String ERROR_RUNNING_BOT_TITLE = "Fatal Error.";
+    
 
     /**
      * Generate messages for the message dialog box.
